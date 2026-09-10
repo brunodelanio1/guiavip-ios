391cd5c4f609cce558e788d7d22d31b36393bc38
brunodelanio1 <brunodelanio@hotmail.com>
2026-09-10 13:59:45 -0300

    Update Settings.swift

--- Guia Vip/src/Guia Vip/Settings.swift
+++ Guia Vip/src/Guia Vip/Settings.swift
@@ -8,11 +8,11 @@ struct Cookie {
 let gcmMessageIDKey = "00000000000" // update this with actual ID if using Firebase 
 
 // URL for first launch
-let rootUrl = URL(string: "https://guiavip.pages.dev")!
+let rootUrl = URL(string: "https://guiavip.net")!
 
 // allowed origin is for what we are sticking to pwa domain
 // This should also appear in Info.plist
-let allowedOrigins: [String] = ["guiavip.pages.dev"]
+let allowedOrigins: [String] = ["guiavip.net"]
 
 // auth origins will open in modal and show toolbar for back into the main origin.
 // These should also appear in Info.plist
